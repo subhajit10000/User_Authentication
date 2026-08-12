8cef7ffaeebb103ffd9aff53216da542066d30f5
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-08-12 19:41:39 +0530

    Add files via upload

---
+++ src/utils/hash.js
@@ -0,0 +1,12 @@
+import bcrypt from "bcrypt";
+import env from "../config/env.js";
+
+const hashPassword = async (password) => {
+    return await bcrypt.hash(password, Number(env.BCRYPT_SALT_ROUNDS));
+};
+
+const comparePassword = async (password, hashedPassword) => {
+    return await bcrypt.compare(password, hashedPassword);
+};
+
+exports = { hashPassword, comparePassword };
