78fd5f4a01d38a89834b6677baa90f0cb0499d72
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-08-29 09:36:31 +0530

    Add files via upload

--- src/database/index.js
+++ src/database/index.js
@@ -1,16 +1,20 @@
 import mongoose from "mongoose";
+
 import env from "../config/env.js";
 import logger from "../utils/logger.js";
 const connectDB = async () => {
     try {
-        
+
         const connection = await mongoose.connect(env.MONGO_URI);
 
-        logger.info(`MongoDB Connected`)
+        logger.info('MongoDB Connection done.')
+
+
     } catch (error) {
         logger.error(error.message);
         process.exit(1);
     }
-};
+}
+
 
 export default connectDB;
