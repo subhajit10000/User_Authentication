217b468418d7c05ac96c94aa767e81affb219919
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-08-30 10:51:56 +0530

    Add files via upload

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -1,16 +1,23 @@
-import {validationResult} from 'express-validator';
-import ApiError from '../utils/apiError.js';
+import { validationResult } from "express-validator";
+import ApiError from "../utils/ApiError.js";
+
+
 
 const validate = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        const formattedErrors = errors.array().map((err) => ({ 
-            field: err.path, 
-            message: err.msg 
+        const formattedErrors = errors.array().map((err) => ({
+            field: err.path,
+            message: err.msg
         }));
+
         return next(
-            new ApiError(422, 'Validation failed', formattedErrors));
+            new ApiError(422, "Validaton failed", formattedErrors)
+        )
     }
+
     next();
-};
+}
+
+
 export default validate;
