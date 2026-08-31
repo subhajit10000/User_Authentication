608898a845c08582af1399dfae9655e131c67d96
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-08-31 20:00:38 +0530

    Add files via upload

--- src/models/User.js
+++ src/models/User.js
@@ -1,47 +1,43 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
-import validator from "validator";
+import validator from "validator"
 
-import {generateAccessToken, generateRefreshToken} from "../config/jwt.js";
 
-const userSchema = new mongoose.Schema( 
+import { generateAccessToken, generateRefreshToken } from "../config/jwt.js";
+
+const userSchema = new mongoose.Schema(
     {
         firstName: {
             type: String,
             required: true,
             trim: true,
             minlength: 2,
-            maxlength: 30,
+            maxlength: 30
         },
         lastName: {
             type: String,
             required: true,
             trim: true,
             minlength: 2,
-            maxlength: 30,  
+            maxlength: 30
         },
         email: {
             type: String,
             required: true,
             trim: true,
-            unique: true,
             lowercase: true,
-            validate(value) {
-                if (!validator.isEmail(value)) {
-                    throw new Error("Invalid email");
-                }
-            }
+            unique: true,
+            validate: [validator.isEmail, "Invalid Email"],
         },
         password: {
             type: String,
             required: true,
             minlength: 8,
-            select: false,
+            select: false
         },
         phone: {
             type: String,
-            required: true,
-            default: ""
+            default: null
         },
         role: {
             type: String,
@@ -56,35 +52,61 @@ const userSchema = new mongoose.Schema(
             type: Boolean,
             default: false
         },
-        lastLogin: {
+
+
+        otp: {
+            type: String,
+            select: false,
+            default: null
+        },
+        otpExpiry: {
             type: Date,
-        }   
+            select: false,
+            default: null
+        },
+        otpPurpose: {
+            type: String,
+            enum: ["verifyEmail", "resetPassword", null],
+            select: false,
+            default: null
+        },
+
+        lastLogin: Date,
     },
     {
         timestamps: true
     }
 );
 
-userSchema.pre("save", async function(next) {
-    if (!this.isModified("password")) {
-        return next();
-    }
+
+userSchema.pre("save", async function () {
+    if (!this.isModified('password')) return;
+
     this.password = await bcrypt.hash(this.password, 12);
-    next();
 });
 
-userSchema.methods.comparePassword = async function(Password) {
-    return await bcrypt.compare(Password, this.password);
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return generateAccessToken({
+        id: this._id,
+        email: this.email,
+        role: this.role,
+    })
 };
 
-userSchema.methods.generateAccessTokens = function() {
-    return generateAccessToken({ id: this._id, email: this.email, role: this.role });
+userSchema.methods.generateRefreshToken = function () {
+    return generateRefreshToken({
+        id: this._id
+    });
 };
 
-userSchema.methods.generateRefreshTokens = function() {
-    return generateRefreshToken({ id: this._id});
-};
 
 const User = mongoose.model("User", userSchema);
 
 export default User;
+
+
