0cfeeb0060e64319cc0ab410df4c8eb88a998b91
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-08-31 20:02:05 +0530

    Add files via upload

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -1,24 +1,54 @@
-import User from "../models/user.js";
+
+import User from "../models/User.js";
+
 const createUser = async (userData) => {
-  return await User.create(userData);
-};
+    return await User.create(userData);
+}
 
 const findUserByEmail = async (email) => {
-  return await User.findOne({ email }).select("+password");
-};
+    return await User.findOne({ email }).select("+password");
+}
+
+// Login uses findUserByEmail (needs +password). OTP flows need the OTP
+// fields too, which are select:false by default.
+const findUserByEmailWithOTP = async (email) => {
+    return await User.findOne({ email }).select("+otp +otpExpiry +otpPurpose");
+}
+
 const findUserById = async (id) => {
-  return await User.findById(id);
+    return await User.findById(id);
+}
+
+const findAllUsers = async () => {
+  return await User.find({})
+    .select("-password")
+    .sort({ createdAt: -1 });
 };
 
+// changePasswordService needs the current hashed password to compare against.
+const findUserByIdWithPassword = async (id) => {
+    return await User.findById(id).select("+password");
+}
+
 const updateUser = async (id, data) => {
-  return await User.findByIdAndUpdate(id, data, {
-    new: true,
-    runValidators: true,
-  });
+    return await User.findByIdAndUpdate(id, data, {
+        new: true,
+        runValidators: true
+    })
 };
 
 const deleteUser = async (id) => {
-  return await User.findByIdAndDelete(id);
-};
+    return await User.findByIdAndDelete(id);
+}
 
-export { createUser, findUserByEmail, findUserById, updateUser, deleteUser };
+export {
+    createUser,
+    findUserByEmail,
+    findUserByEmailWithOTP,
+    findUserById,
+    findUserByIdWithPassword,
+    updateUser,
+    deleteUser,
+    findAllUsers
+}
+
