a78a5b9481d4145343ed0e9ec59b8b10d022c1b0
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-09-01 19:31:57 +0530

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,9 +1,46 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import morgan from "morgan";
+import env from "./config/env.js";
+import authRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/user.routes.js"
+import { apiLimiter } from "./middlewares/rateLimiter.js";
+import errorHandler from "./middlewares/error.middleware.js"
 
 const App = express();
+App.use(helmet()); // security
 
-App.get("/", (req, res) => {
-    res.send("Hello, World!");
-});
+App.use(
+    cors({
+        origin: env.CLIENT_URL,
+        credentials: true,
+    })
+);
+App.use(apiLimiter);
+App.use(express.json({ limit: '50kb' }));
+App.use(express.urlencoded({ extended: true }))
 
-export default App
+App.use(cookieParser());
+App.use(compression());
+
+if (env.NODE_ENV === "development") {
+    App.use(morgan("dev"));
+}
+App.get('/', (req, res) => {
+    res.send('Authentication API running')
+})
+App.use("/api/v1/auth", authRouter);
+App.use("/api/v1/users", userRouter);
+
+App.use((req, res) => {
+    res.status(404).json({
+        success: false,
+        message: "Route not found"
+    })
+})
+
+App.use(errorHandler);
+export default App;
