c845e9afdd9771f7e75aeedb63b8f9e07cfedcb8
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-09-02 09:34:41 +0530

    Add files via upload

--- src/utils/decrypt.js
+++ src/utils/decrypt.js
@@ -1,16 +1,16 @@
 import crypto from "crypto";
 import env from "../config/env.js";
 
-const key = Buffer.from(env.CRYPTO_SECRET, "utf-8");
-const iv = Buffer.from(env.CRYPTO_IV, "utf-8");
+const key = Buffer.from(env.CRYPTO_SECRET, 'utf-8');
+const iv = Buffer.from(env.CRYPTO_IV, 'utf-8');
 const algorithm = env.CRYPTO_ALGORITHM;
+
 
 export const decrypt = (encryptedText) => {
     if (!encryptedText) return "";
-
     const decipher = crypto.createDecipheriv(algorithm, key, iv);
     let decrypted = decipher.update(encryptedText, "hex", "utf-8");
-    decrypted += decipher.final("utf-8");
 
+    decrypted += decipher.final("utf-8");
     return decrypted;
-};
+}
