58d154d8cc0d4a01997629f564da7bbf8836f78b
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-09-02 09:35:40 +0530

    Add files via upload

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,22 +1,21 @@
 class ApiError extends Error {
-  constructor(statusCode, message = "something went wrong", errors = []) {
+  constructor(statusCode, message = "Something went wrong", errors = []) {
     super(message);
 
-    this.statusCode = statusCode;
-    this.errors = errors;
     this.success = false;
+    this.statusCode = statusCode;
     this.message = message;
-    this.timestamp = new Date().toLocaleString("en-IN", {
-      timeZone: "Asia/Kolkata",
-      day: "2-digit",
-      month: "2-digit",
-      year: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-      hour12: true, // Set to false if you want 24-hour format
-    });
-
+    this.errors = errors;
+    this.timestamp = new Date().toLocaleString('en-IN', {
+    timeZone: 'Asia/Kolkata',
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: true 
+  });
     Error.captureStackTrace(this, this.constructor);
   }
 }
