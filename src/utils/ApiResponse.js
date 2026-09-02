498848285cc2186f637ef348c3347e37db61eb91
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-09-02 09:35:28 +0530

    Add files via upload

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,20 +1,20 @@
 class ApiResponse {
-  constructor(statusCode, message = "success", data = null) {
-    this.statusCode = statusCode < 400;
+  constructor(statusCode, message = "Success", data = null) {
+    this.success = statusCode < 400;
     this.statusCode = statusCode;
     this.message = message;
     this.data = data;
 
     this.timestamp = new Date().toLocaleString('en-IN', {
-      timeZone: 'Asia/Kolkata',
-      day: '2-digit',
-      month: '2-digit',
-      year: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-      hour12: true // Set to false if you want 24-hour format
-    });
+    timeZone: 'Asia/Kolkata',
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: true 
+  });
   }
 }
 
