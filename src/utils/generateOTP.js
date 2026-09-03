6739066255d1a2c2df705d45856f72d8014063fe
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-09-03 19:41:58 +0530

    Add files via upload

--- src/utils/generateOTP.js
+++ src/utils/generateOTP.js
@@ -1,12 +1,15 @@
 import crypto from "crypto";
 import env from "../config/env.js";
 
-const generateOTP = (length = Number (env.OTP_LENGTH) || 6 ) => {   
-    const digits = "0123456789";
-    let otp = "";   
+const generateOTP = (length = Number(env.OTP_LENGTH) || 6) => {
+    const digit = "0123456789";
+    let otp = "";
     while (otp.length < length) {
-        const randomIndex = crypto.randomInt(0, digits.length);
-        otp += digits[randomIndex];
+        const randomIndex = crypto.randomInt(0, digit.length);
+        otp += digit[randomIndex];
     }
+
     return otp;
-};
+}
+
+export default generateOTP;
