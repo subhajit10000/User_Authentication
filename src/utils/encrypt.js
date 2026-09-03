3d3afa3444f9854f8736b111b0427f1ac0fb6f73
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-09-03 19:41:47 +0530

    Add files via upload

--- src/utils/encrypt.js
+++ src/utils/encrypt.js
@@ -1,16 +1,19 @@
 import crypto from "crypto";
 import env from "../config/env.js";
 
-const key = Buffer.from(env.CRYPTO_SECRET, "utf-8");
-const iv = Buffer.from(env.CRYPTO_IV, "utf-8");
+const key = Buffer.from(env.CRYPTO_SECRET, 'utf-8');
+const iv = Buffer.from(env.CRYPTO_IV, 'utf-8');
 const algorithm = env.CRYPTO_ALGORITHM;
-
 
 export const encrypt = (text) => {
     if (!text) return "";
-
-    const cipher = crypto.createCipheriv(algorithm, key, iv);
-    let encrypted = cipher.update(text, "utf-8", "hex");
-    encrypted += cipher.final("hex");   
+    const cipher = crypto.createCipheriv(
+        algorithm,
+        key,
+        iv
+    );
+    let encrypted = cipher.update(text, 'utf-8', "hex");
+    encrypted += cipher.final("hex");
     return encrypted;
-};  
+
+}
